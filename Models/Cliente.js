9642daeed5e61db62514c9b70d1138f1e2125cc8
Yan Boldrini <yan.hipamo11@gmail.com>
2023-01-31 17:57:48 -0400

    Implementando class Cliente, com 3 atributos.

---
+++ Models/Cliente.js
@@ -0,0 +1,25 @@
+function Cliente() {
+    var Nome;
+    var Idade;
+    var Cpf;
+
+
+    this.getNome = function () {
+        return Nome;
+    };
+    this.setNome = function (value) {
+        Nome = value;
+    };
+    this.getIdade = function () {
+        return Idade;
+    };
+    this.setIdade = function (value) {
+        Idade = value;
+    };
+    this.getCpf = function (){
+        return Cpf;
+    }
+    this.setCpf = function (value){
+        CPF = value;
+    }
+}
