2ebcd0ee3470f860e4da7bce2c3bf57bdfdd8c53
Yan Boldrini <yan.hipamo11@gmail.com>
2023-02-08 19:17:23 -0400

    eventos de form login.

---
+++ Views/loginView.js
@@ -0,0 +1,27 @@
+class LoginView{
+    constructor() {
+        this.loginForm = document.getElementById('login-form');
+        this.usernameInput = document.getElementById('user');
+        this.passwordInput = document.getElementById('pass');
+        this.erroMessage = document.getElementById('error-message');
+    }
+    showError (message){
+        this.erroMessage.textContent = message;
+        this.erroMessage.style.display = 'block';
+    }
+    hideError (){
+        this.erroMessage.style.display = 'nome';
+    }
+    clearForm(){
+        this.usernameInput.value ='';
+        this.passwordInput.value ='';
+    }
+    onSubmit (calback){
+        this.loginForm.addEventListener('submit', event => {
+            event.preventDefault();
+            const user = this.usernameInput.value;
+            const pass = this.passwordInput.value;
+            calback(user,pass);
+        })
+    }
+}
