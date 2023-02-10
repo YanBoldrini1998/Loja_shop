8c116714a155ae9086dda1198213a6e513739fe7
Julianapvh <70443877+julianapvh@users.noreply.github.com>
2023-02-10 19:34:43 -0400

    tela de cadastro

--- Views/JS/cadastroView.js
+++ Views/JS/cadastroView.js
@@ -4,7 +4,7 @@ function limpa_formulário_cep() {
     document.getElementById('bairro').value = ("");
     document.getElementById('cidade').value = ("");
     document.getElementById('uf').value = ("");
-    document.getElementById('ibge').value = ("");
+   
 }
 
 function meu_callback(conteudo) {
@@ -14,7 +14,7 @@ function meu_callback(conteudo) {
         document.getElementById('bairro').value = (conteudo.bairro);
         document.getElementById('cidade').value = (conteudo.localidade);
         document.getElementById('uf').value = (conteudo.uf);
-        document.getElementById('ibge').value = (conteudo.ibge);
+       
     } //end if.
     else {
         //CEP não Encontrado.
@@ -42,7 +42,7 @@ function pesquisacep(valor) {
             document.getElementById('bairro').value = "...";
             document.getElementById('cidade').value = "...";
             document.getElementById('uf').value = "...";
-            document.getElementById('ibge').value = "...";
+            
 
             //Cria um elemento javascript.
             var script = document.createElement('script');
